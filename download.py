b8de7cf3ba7e386e46bc346490db31bae057791e
MBtech <mbilal_ce@live.com>
2015-04-29 12:06:26 +0200

    Prompt user for password instead of reading from clear text file

--- download.py
+++ download.py
@@ -5,27 +5,36 @@ GOOGLE_LOGIN = GOOGLE_PASSWORD = AUTH_TOKEN = None
 
 import sys
 from pprint import pprint
-
+import getpass
 from config import *
 from googleplay import GooglePlayAPI
 from helpers import sizeof_fmt
 
 if (len(sys.argv) < 2):
-    print "Usage: %s packagename [filename]"
+    print "Usage: %s packagename [filename, password]"
     print "Download an app."
     print "If filename is not present, will write to packagename.apk."
+    print "If password is not present, user will be prompted for one"
     sys.exit(0)
 
 packagename = sys.argv[1]
 
-if (len(sys.argv) == 3):
+if ((len(sys.argv) == 3) and (len(sys.argv)<5)):
     filename = sys.argv[2]
-else:
+    google_pass = sys.argv[3]
+elif (len(sys.argv)<3):
     filename = packagename + ".apk"
+    google_pass = getpass.getpass()
+else:
+    print "Usage: %s packagename filename, password]"
+    print "Download an app."
+    print "If filename is not present, will write to packagename.apk."
+    print "If password is not present, user will be prompted for one"
+    sys.exit(0)
 
 # Connect
 api = GooglePlayAPI(ANDROID_ID)
-api.login(GOOGLE_LOGIN, GOOGLE_PASSWORD, AUTH_TOKEN)
+api.login(GOOGLE_LOGIN, google_pass, AUTH_TOKEN)
 
 # Get the version code and the offer type from the app details
 m = api.details(packagename)
